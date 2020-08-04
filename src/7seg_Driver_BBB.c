d2af9bdffed9f87353bb0ce34945cb32aa7395bb
sharathhvr <sharathhvr@csu.fullerton.edu>
2020-08-04 13:34:11 -0700

    first commit

---
+++ src/7seg_Driver_BBB.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : 7seg_Driver_BBB.c
+ Author      : Sharath
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
